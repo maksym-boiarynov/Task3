9c405e8ccf5835d206a2b6058cd62e7fbc48b166
Maksym Boiarynov <boyarinov.karazin@gmail.com>
2023-09-16 15:56:07 +0300

    Add project files.

---
+++ Task3_2nd term/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Task3_2nd_term;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        User user1 = new User();
+
+        Console.Write("Enter name: ");
+        user1.FirstName = Console.ReadLine();
+
+        Console.Write("Enter surname: ");
+        user1.LastName = Console.ReadLine();
+
+        Console.Write("Enter gender (Where Male is 1 and Female is 2): ");
+        string genderInput = Console.ReadLine();
+        
+        if (Enum.TryParse(genderInput, out GenderType gender))
+        {
+            user1.Gender = gender;
+            Console.WriteLine("Info about person:");
+            Console.WriteLine($"Name: {user1.FirstName}, Surname: {user1.LastName}, Age: {user1.Age}, Gender: {user1.Gender}");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect format of data. Use 1 for Male or 2 for Female.");
+        }
+    }
+}
---
+++ Task3_2nd term/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_2nd_term
+{
+    class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; }
+        public GenderType Gender { get; set; }
+
+        public User()
+        {
+            Age = 21;
+        }
+
+        public User(int age)
+        {
+            Age = age;
+        }
+    }
+    enum GenderType
+    {
+        Male = 1,
+        Female = 2
+    }
+}
